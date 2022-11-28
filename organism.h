4ebc8237b0dbd45bdb4dca8186d36679e27fc604
Iga Janik <iga.rozalia.janik@gmail.com>
2022-11-28 11:41:27 +0100

    Add organism eat another organism

--- organism.h
+++ organism.h
@@ -5,6 +5,10 @@
 #include <cstdint>
 #include <tuple>
 
+// To pewnie nie powinno być definem, ale czym?
+#define plant_energy_loss 1
+#define animal_energy_loss 2
+
 constexpr bool is_movable(bool eats_m, bool eats_p) { return eats_m || eats_p; }
 
 constexpr bool same_food_preferences(bool sp1_eats_m, bool sp1_eats_p,
@@ -85,6 +89,20 @@ constexpr auto make_child(auto organism1, auto organism2) {
     return std::make_tuple(Organism(organism1), Organism(organism2), child);
 }
 
+constexpr auto first_eat_second(auto organism1, auto organism2, int energy_loss) {
+    return std::make_tuple(organism1.fed_with(organism2.get_vitality() / energy_loss),
+                           organism2.dead(), std::nullopt);
+}
+
+constexpr auto second_eat_first(auto organism1, auto organism2, int energy_loss) {
+    return std::make_tuple(organism1.dead(), organism2.fed_with
+                            (organism1.get_vitality() / energy_loss), std::nullopt);
+}
+
+constexpr auto both_die(auto organism1, auto organism2) {
+    return std::make_tuple(organism1.dead(), organism2.dead(), std::nullopt);
+}
+
 template <typename species_t, bool sp1_eats_m, bool sp1_eats_p,
         bool sp2_eats_m, bool sp2_eats_p>
 // Możliwe jest spotkanie jedynie organizmów, których typ gatunku (species_t) jest taki sam
@@ -95,7 +113,6 @@ constexpr std::tuple<Organism<species_t, sp1_eats_m, sp1_eats_p>,
         std::optional<Organism<species_t, sp1_eats_m, sp1_eats_p>>>
 encounter(Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
           Organism<species_t, sp2_eats_m, sp2_eats_p> organism2) {
-
     // Spotkanie, w którym jedna ze stron jest martwa, nie powoduje skutków.
     if ((organism1.is_dead()) || (organism2.is_dead())) {
         return no_effect(organism1, organism2);
@@ -110,12 +127,10 @@ encounter(Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
     // Dokładnie jeden jest rośliną, drugi może go zjeść lub nie.
     if (organism1.is_plant() || organism2.is_plant()) {
         if (sp1_eats_p) {
-            return std::make_tuple
-            (organism1.fed_with(organism2.get_vitality()), organism2.dead(), std::nullopt);
+            return first_eat_second(organism1, organism2, plant_energy_loss);
         }
         else if (sp2_eats_p) {
-            return std::make_tuple
-            (organism1.dead(), organism2.fed_with(organism1.get_vitality()), std::nullopt);
+            return second_eat_first(organism1, organism2, plant_energy_loss);
         }
         else {
             return no_effect(organism1, organism2);
@@ -126,15 +141,13 @@ encounter(Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
     // Oba są mięsożerne, więc walczą.
     if (sp1_eats_m && sp2_eats_m) {
         if (organism1.get_vitality() > organism2.get_vitality()) {
-            return std::make_tuple
-            (organism1.fed_with(organism2.get_vitality() / 2), organism2.dead(), std::nullopt);
+            return first_eat_second(organism1, organism2, animal_energy_loss);
         }
         else if (organism1.get_vitality() < organism2.get_vitality()) {
-            return std::make_tuple
-            (organism1.dead(), organism2.fed_with(organism1.get_vitality() / 2), std::nullopt);
+            return second_eat_first(organism1, organism2, animal_energy_loss);
         }
         else {
-            return std::make_tuple(organism1.dead(), organism2.dead(), std::nullopt);
+            return both_die(organism1, organism2);
         }
     }
 
@@ -144,8 +157,7 @@ encounter(Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
             return no_effect(organism1, organism2);
         }
         else {
-            return std::make_tuple
-            (organism1.fed_with(organism2.get_vitality() / 2), organism2.dead(), std::nullopt);
+            return first_eat_second(organism1, organism2, animal_energy_loss);
         }
     }
 
@@ -154,8 +166,7 @@ encounter(Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
             return no_effect(organism1, organism2);
         }
         else {
-            return std::make_tuple
-            (organism1.dead(), organism2.fed_with(organism1.get_vitality() / 2), std::nullopt);
+            return second_eat_first(organism1, organism2, animal_energy_loss);
         }
     }
 
